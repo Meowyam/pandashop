26bca9cd6ffe0800b2bf6284798d878ea38b391f
Meowyam <maryam@chilon.net>
2020-07-05 21:28:43 +0800

    added google analytics and seo

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,11 +15,14 @@ export default {
   ** See https://nuxtjs.org/api/configuration-head
   */
   head: {
-    title: process.env.npm_package_name || '',
+    title: 'Pandashop',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+      { name: 'google-site-verification', content: 'PgyvKaiR1h1Pjub4OlZewDnm0zhQ69fBlgEn0wo1rro' },
+      { name: 'copyright', content: 'Maryam Hanafiah of chilon.net ltd' },
+      { hid: 'description', name: 'description', content: 'Hi! I\'m a creative technologist, javascript and python developer based in London and Singapore. This is my personal portfolio and resume.' },
+      { name: 'keywords', content: 'web developer, creative technologist, vuejs, nuxtjs, angularjs, python, php, wordpress, raspberry pi, ui, illustration, illustrator, layout designer, html, tech, tech blog, javascript, es6, docker, kubernetes, ballerinaio' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
@@ -50,6 +53,10 @@ export default {
   ** Nuxt.js dev-modules
   */
   buildModules: [
+    ['@nuxtjs/google-analytics', {
+      id: 'UA-28226014-1'
+    }]
+
   ],
   /*
   ** Nuxt.js modules
